33468fab80eb25ffd790d7c488351c8c3188c229
Tim Boudreau <tim@timboudreau.com>
2017-12-14 04:21:29 -0500

    Readme, docs, license, a couple of sanity checks

---
+++ README.md
@@ -0,0 +1,5 @@
+Media-Samples
+-------------
+
+Just some media files bundled into a Java library for use in tests.
+
--- src/main/java/mlo/media/samples/Media.java
+++ src/main/java/mlo/media/samples/Media.java
@@ -13,25 +13,49 @@ import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 
 /**
+ * Enum of media file samples for use in selenium tests.
  *
  * @author Tim Boudreau
  */
 public enum Media {
 
-    MPEG_4("000B.mp4"),
-    MPEG_1("awful-mpeg1.mpg"),
-    PNG("ball-unc.png"),
-    GIF("gif.gif"),
-    GARAGE_BAND_M4A("inergy-2.m4a"),
-    TIFF("MildredScarcliff1980.tiff"),
-    AVI_MS_VIDEO_1("msvideo1-avi.avi"),
-    WAV("Mama-Lolo.wav");
+    MPEG_4("000B.mp4", 8720308),
+    MPEG_1("awful-mpeg1.mpg", 161177),
+    PNG("ball-unc.png", 89707),
+    GIF("gif.gif", 1512),
+    GARAGE_BAND_M4A("inergy-2.m4a", 7698985),
+    TIFF("MildredScarcliff1980.tiff", 3571332),
+    OGG_AUDIO("mil.oga", 873954),
+    AVI_MS_VIDEO_1("msvideo1-avi.avi", 962334),
+    WAV("Mama-Lolo.wav", 19553064);
 
     private final String name;
+    private final int length;
     private Path file;
 
-    Media(String name) {
+    Media(String name, int length) {
         this.name = name;
+        InputStream in = toInputStream();
+        try {
+            if (in == null) {
+                throw new Error(name + " is not on the classpath relative to : " + Media.class.getName());
+            }
+        } finally {
+            try {
+                in.close();
+            } catch (Exception ex) {
+                throw new Error(ex);
+            }
+        }
+        this.length = length;
+    }
+
+    public long length() {
+        return length;
+    }
+
+    public String fileName() {
+        return name;
     }
 
     public synchronized Path toPath() throws IOException {
@@ -40,24 +64,28 @@ public enum Media {
         }
         file = new File(System.getProperty("java.io.tmpdir")).toPath().resolve(name);
         if (!Files.exists(file)) {
-            System.out.println("COPY TO " + file);
             copyBytesFromClasspath(file);
         }
         return file;
     }
 
+    public synchronized InputStream toInputStream() {
+        return Media.class.getResourceAsStream(name);
+    }
+
     public String toString() {
-        return name;
+        return fileName();
     }
 
     private void copyBytesFromClasspath(Path file) throws IOException {
-        try (InputStream in = Media.class.getResourceAsStream(name)) {
+        try (InputStream in = toInputStream()) {
             Files.copy(in, file, StandardCopyOption.REPLACE_EXISTING);
         }
     }
 
     public static void main(String... args) throws IOException {
-        Path p = Media.PNG.toPath();
-        System.out.println("GOT " + p);
+        for (Media m : Media.values()) {
+            System.out.println(m + " -> " + m.toPath());
+        }
     }
 }
